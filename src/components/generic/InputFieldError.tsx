bc63f3fbbd650acfa9eb9de889f111eec50e2991
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-08-08 01:14:45 +0900

    feat : .gitignore추가, 기본 컴포넌트 작성

---
+++ src/components/generic/InputFieldError.tsx
@@ -0,0 +1,22 @@
+// 인풋필드의 에러를 띄우는 컴포넌트
+import { clsx } from 'clsx'
+import type { CommonVariant, VariantStyleMap } from '../../types'
+
+type InputFieldErrorProps = {
+    variant: CommonVariant 
+    message: string
+    isVisible?: boolean //  조건부 렌더링 요소
+}
+
+export default function InputFieldError({ message, variant, isVisible }: InputFieldErrorProps) {
+    if (!isVisible || !message) return null // 에러 안받으면 안보임
+
+    const variantStyle:VariantStyleMap = {
+        generic: '', //일반 스타일
+    }
+    return (
+        <span className={clsx(variantStyle[variant]??'')}>
+            {message}
+        </span>
+    )
+}
