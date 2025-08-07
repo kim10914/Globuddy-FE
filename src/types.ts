1e092949b2463c386f074428d88453e7fd8b4f1f
SUNG_HWAN <43512783+kim10914@users.noreply.github.com>
2025-08-08 01:16:06 +0900

    refactor : 주석 수정

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@
 // 일반적인 적용 | 00 페이지
 export type CommonVariant = 'generic' | 'modifyPage'
 
-// generic은 필수요건 나머지는 선택 -> variantStyle[variant]??'' callback 처리 필요
+// generic은 필수요소 나머지는 선택 -> variantStyle[variant]??'' callback 처리 필요
 export type VariantStyleMap = {
     generic: string
 } & Partial<Record<Exclude<CommonVariant, 'generic'>, string>>
