a6ea742573c2846905fdd3d46c063b4d5b17c513
hwido <kingluminance@gmail.com>
2025-08-09 03:45:38 +0900

    feat/App.js 초기 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
 function App() {
+  const [islogin, setIslogin] = useState(false); //로그인 상태 저장 state
+
   return (
-    <div>
-      페이지
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<div>홈</div>} />
+      <Route
+        path=""
+        element={
+          islogin ? (
+            <Routes>
+              <Route></Route>
+            </Routes>
+          ) : (
+            <Navigate to="/login" replace /> //자동으로 로그인 페이지로 보내는 컴포넌트 입니다, replace로 옵션으로 뒤로가기 막음
+          )
+        }
+      ></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
