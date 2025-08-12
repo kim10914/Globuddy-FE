37238115d1b40823967ac7f90688b95bb829898a
hwido <kingluminance@gmail.com>
2025-08-12 19:36:07 +0900

    feat:주석추가

--- src/utils/google-login/authCallbackHandler.tsx
+++ src/utils/google-login/authCallbackHandler.tsx
@@ -1,13 +1,13 @@
-//구글 OAuth 로그인 후 콜백 페이지 역할
+//구글 OAuth 로그인 후 콜백페이지에서 파라미터 추출
 export async function authCallbackHandler() {
-  const params = new URLSearchParams(window.location.search);
+  const params = new URLSearchParams(window.location.search); //현재 페이지 url의 파라미터를 조회
   const code = params.get("code");
   console.log("구글 콜백 URL의 code 파라미터:", code);
   if (!code) {
     console.error("Authorization code not found");
     return;
   }
-  //백엔드 요청 부분 , axios 적용 고려 , 스웨거 맞춰 수정 필요 + api파일로 분리,
+  //백엔드 요청 부분 , axios 적용 고려 , 스웨거 맞춰 수정 후 api파일로 분리 필요,
   const response = await fetch("/api/auth/google", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
