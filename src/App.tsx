5a42345e65cc3e0a8c5a173a09b5fdcf2ebf6bcb
hwido <kingluminance@gmail.com>
2025-08-14 01:59:40 +0900

    feat:마이페이지 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
+import MyPage from "./pages/Mypage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 function App() {
@@ -9,13 +10,13 @@ function App() {
     <Routes>
       <Route path="/" element={<div>홈</div>} />
       <Route path="/login" element={<Signup />} />
-      <Route path="/auth/callback" element={<AuthCallbackPage />} />
+      <Route path="/oauth/google/redirect" element={<AuthCallbackPage />} />
       <Route
         path="/*"
         element={
           islogin ? ( //로그인 상태시 접근 가능 페이지들 입니다
             <Routes>
-              <Route></Route>
+              <Route path="/mypage" element={<MyPage />}></Route>
             </Routes>
           ) : (
             <Navigate to="/login" replace /> //자동으로 로그인 페이지로 보내는 컴포넌트 입니다, replace로 옵션으로 뒤로가기 막음
