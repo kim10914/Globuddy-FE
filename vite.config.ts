df8fd2aaf71f2aa990aff51382ed20cd96e31ead
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-08-14 15:57:35 +0900

    fix : 플러그인 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'; // SVG 컴포넌트화
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss()],
 })
