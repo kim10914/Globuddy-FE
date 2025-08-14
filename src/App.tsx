08b3dc92510e986ea05b05cf869c669e51280c60
hwido <kingluminance@gmail.com>
2025-08-15 05:02:37 +0900

    feat:마이페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
-import MyPage from "./pages/Mypage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import ProfilePage from "./pages/mypage/ProfilePage";
+import SettingPage from "./pages/mypage/SettingsPage";
 
 function App() {
-  const islogin: boolean = false;
+  const islogin: boolean = true;
 
   return (
     <Routes>
@@ -16,7 +17,8 @@ function App() {
         element={
           islogin ? ( //로그인 상태시 접근 가능 페이지들 입니다
             <Routes>
-              <Route path="/mypage" element={<MyPage />}></Route>
+              <Route path="/mypage" element={<ProfilePage />}></Route>
+              <Route path="/mypage/setting" element={<SettingPage />}></Route>
             </Routes>
           ) : (
             <Navigate to="/login" replace /> //자동으로 로그인 페이지로 보내는 컴포넌트 입니다, replace로 옵션으로 뒤로가기 막음
