361af8a9127462311aa830811200cf890d1cfbe3
hwido <kingluminance@gmail.com>
2025-08-20 14:11:12 +0900

    fix : Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import Signup from "./pages/login-page/Signup";
 import AuthCallbackPage from "./pages/login-page/AuthCallbackPage";
 import MainPage from "./pages/MainPage";
@@ -60,39 +60,21 @@ function App() {
 
       <Route
         path="/*"
-        element={
-          isLoggedIn() ? ( //로그인 상태시 접근 가능 페이지들 입니다
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/profile" element={<ProfilePage />}></Route>
-              <Route path="/profile/setting" element={<SettingPage />}></Route>
-              <Route path="/community" element={<Community />} />
-              <Route path="/board/:category" element={<CommunityCategory />} />
-              <Route
-                path="/board/:category/:postId"
-                element={<CommunityPost />}
-              />
-              <Route
-                path="/board/:category/write"
-                element={<CommunityWrite />}
-              />
-              <Route
-                path="/board/:category/mine"
-                element={<CommunityMyPost />}
-              />
-              <Route path="/road-map" element={<RoadmapFlights />} />
-              <Route path="/visa/:country" element={<VisaSearch />} />
-              <Route path="/road-map/write" element={<VisaInfo />} />
-              <Route
-                path="/profile/notifications"
-                element={<NotificationsPage />}
-              ></Route>
-            </Routes>
-          ) : (
-            <Navigate to="/login" replace /> //자동으로 로그인 페이지로 보내는 컴포넌트 입니다, replace로 옵션으로 뒤로가기 막음
-          )
-        }
-      ></Route>
+        element={isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />}
+      >
+        <Route path="/" element={<MainPage />} />
+        <Route path="/profile" element={<ProfilePage />}></Route>
+        <Route path="/profile/setting" element={<SettingPage />}></Route>
+        <Route path="/community" element={<Community />} />
+        <Route path="/board/:category" element={<CommunityCategory />} />
+        <Route path="/board/:category/:postId" element={<CommunityPost />} />
+        <Route path="/board/:category/write" element={<CommunityWrite />} />
+        <Route path="/board/:category/mine" element={<CommunityMyPost />} />
+        <Route path="/road-map" element={<RoadmapFlights />} />
+        <Route path="/visa/:country" element={<VisaSearch />} />
+        <Route path="/road-map/write" element={<VisaInfo />} />
+        <Route path="/profile/notifications" element={<NotificationsPage />} />
+      </Route>
     </Routes>
   );
 }
